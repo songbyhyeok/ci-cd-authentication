f67e3afcf1979671cb8287bdf368994fcde8f50c
WhiteBear <63230518+songbyhyeok@users.noreply.github.com>
2025-02-27 00:58:55 +0900

    chore/#03/ec2, rds 터널링

---
+++ src/main/java/com/project/cicd_auth/CicdAuthApplication.java
@@ -0,0 +1,13 @@
+package com.project.cicd_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cicd_auth/CicdAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cicd_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
