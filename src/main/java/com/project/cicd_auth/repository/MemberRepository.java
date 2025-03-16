2c589f0992c37e2e486ce48b04a35c24b79315f1
WhiteBear <63230518+songbyhyeok@users.noreply.github.com>
2025-03-16 23:31:31 +0900

    회원가입 로직, 기본 시큐리티 설정 추가

---
+++ src/main/java/com/project/cicd_auth/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.project.cicd_auth.repository;
+
+import com.project.cicd_auth.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Boolean existsByUsername(String username);
+}
---
+++ src/main/java/com/project/cicd_auth/service/JoinService.java
@@ -0,0 +1,41 @@
+package com.project.cicd_auth.service;
+
+import com.project.cicd_auth.dto.JoinDTO;
+import com.project.cicd_auth.entity.Member;
+import com.project.cicd_auth.repository.MemberRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(MemberRepository memberRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+
+        this.memberRepository = memberRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = memberRepository.existsByUsername(username);
+
+        if (isExist) {
+
+            return;
+        }
+
+        Member data = new Member();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        memberRepository.save(data);
+    }
+}
